68232f8b788742e6bcb3e481f437b81a2214ac0a
Jerry Orta <jerryorta.dev@gmail.com>
2023-05-05 19:54:13 -0500

    create storybook config

--- apps/flights/booking/.storybook/main.ts
+++ apps/flights/booking/.storybook/main.ts
@@ -1,20 +1,12 @@
-import type { StorybookConfig } from '@storybook/angular';
+import type {StorybookConfig} from '@storybook/angular';
 
 const config: StorybookConfig = {
-  stories: [
-    '../../../../apps/**/*.stories.@(js|jsx|ts|tsx|mdx)',
-    '../../../../libs/**/*.stories.@(js|jsx|ts|tsx|mdx)',
-  ],
-  // stories: ['../src/app/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+  stories: ['../src/app/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
   addons: ['@storybook/addon-essentials'],
   framework: {
     name: '@storybook/angular',
-    options: {},
-  },
-  docs: {
-    autodocs: true,
-    defaultName: 'Docs', // set to change the name of generated docs entries
-  },
+    options: {}
+  }
 };
 
 export default config;
--- libs/shared/ui-design-library/src/lib/components/my-button/my-button.component.stories.ts
+++ libs/shared/ui-design-library/src/lib/components/my-button/my-button.component.stories.ts
@@ -1,26 +1,21 @@
-import { Meta, moduleMetadata } from '@storybook/angular';
+import { Meta } from '@storybook/angular';
 import { MyButtonComponent } from './my-button.component';
 
 export default {
   title: 'libs/shared/ui-design-library',
   component: MyButtonComponent,
-  // decorators: [
-  //   moduleMetadata({
-  //     imports: [],
-  //   })
-  // ],
 } as Meta<MyButtonComponent>;
 
 export const myButton = {
   render: (args: MyButtonComponent) => ({
-    props: args,
+    props: args
   }),
   args: {
-    primary:  false,
-    backgroundColor:  '#ff00ff',
-    size:  'medium',
-    label:  'Button',
-  },
+    primary: false,
+    backgroundColor: '#ff00ff',
+    size: 'medium',
+    label: 'Button'
+  }
 };
 
 
